a8c8c48b30aec777846224307600458bb8f72343
rhirne12 <126825699+rhirne12@users.noreply.github.com>
2023-04-11 11:52:37 -0400

    complete
    project complete for Physics

--- Assets/Scripts/AIShell.cs
+++ Assets/Scripts/AIShell.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIShell : MonoBehaviour
-{
+public class AIShell : MonoBehaviour {
+
     public GameObject explosion;
+    Rigidbody rb;
 
+    void OnCollisionEnter(Collision col) {
 
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "tank")
-        {
+        if (col.gameObject.tag == "tank") {
+            Debug.Log("Hit tank");
             GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
             Destroy(exp, 0.5f);
             Destroy(this.gameObject);
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+
+        this.transform.forward = rb.velocity;
     }
 }
